dea2305365bcb7c0a20698d6682a6d33de7a7123
Maleur <anna@burti.lv>
2019-12-13 20:12:11 +0200

    Update specials video list

--- src/assets/samples/specialData.js
+++ src/assets/samples/specialData.js
@@ -4,41 +4,41 @@ export default [
     name: 'Fake',
     icon:
     'https://neverthink.tv/assets/images/63e3939725b3d92af5e7b8429a0f4d57e6be661abf380b39348f360e528dd6e2.png',
-    playlist: ['QSqIG5Dl-SM', 'jM0GePXOdT0', 'exLTGu_c5fs',  'Km8kIX-8hVs', 'c9EOCt9kkUo', '-goTfMUabxc', 'y7pZzp99Jgs', '85RhW75xM8U', 'URLyBDYHoGo']
+    playlist: ['ZX8dcmXj0Uo', '8OspgTblGAQ', '8OspgTblGAQ', 'YXu289P-dD4', 'MwJTT1D2PbE']
   },
   {
     id: 2,
     name: 'DJ',
     icon:
     'https://neverthink.tv/assets/images/fde01ee47dc02d83892c35c22f2efd81f52c37edc4f3651be40094a115c812fd.png',
-    playlist: ['FOH3ZOMBwhY', 'ErfEnD2WA3A', 'JZnlJ2upJv8', 'Km8kIX-8hVs']
+    playlist: ['s-v3dXosB_4', 'P2ppuFwPt6A']
   },
   {
     id: 3,
     name: 'Stop  Motion',
     icon:
     'https://neverthink.tv/assets/images/61d1aeee19fd7cff13a8b17727f1b5a4e9645f16c42ff376a5e5f3ce8a373df2.png',
-    playlist: ['_Czxy3nya8Y', 'p8UR4dODogI', 'HoL1csZPYsk', '8V0HETilr4I', 'tHa260XXH6U', 'ADrBo7u3tR4', 'BgZh5T4nG_w', 'J3iSEq5Apfg', 'iCc5l8iWUZs']
+    playlist: ['hVOUgCGqem0', 'tZqIQmdSa1E', '2s4GMLkTNv0', 'LjN0YsSxOmg']
   },
   {
     id: 4,
     name: 'Sad Cooking',
     icon:
     'https://neverthink.tv/assets/images/fde01ee47dc02d83892c35c22f2efd81f52c37edc4f3651be40094a115c812fd.png',
-    playlist: ['FOH3ZOMBwhY', 'ErfEnD2WA3A', 'JZnlJ2upJv8', 'Km8kIX-8hVs']
+    playlist: ['cUW_bXll6YM', 'E928mxLwc1M', 'BfHIw2iFvR4', 'GFjstQ30RI8']
   },
   {
     id: 5,
     name: 'Urban Legends',
     icon:
     'https://neverthink.tv/assets/images/fde01ee47dc02d83892c35c22f2efd81f52c37edc4f3651be40094a115c812fd.png',
-    playlist: ['FOH3ZOMBwhY', 'ErfEnD2WA3A', 'JZnlJ2upJv8', 'Km8kIX-8hVs']
+    playlist: ['OkXqLYPqTDo', 'ZxINaXbsZOw', 'I2O7blSSzpI', 'mOEiA6OK9Eo']
   },
   {
     id: 6,
     name: 'NoNoYes',
     icon:
     'https://neverthink.tv/assets/images/fde01ee47dc02d83892c35c22f2efd81f52c37edc4f3651be40094a115c812fd.png',
-    playlist: ['FOH3ZOMBwhY', 'ErfEnD2WA3A', 'JZnlJ2upJv8', 'Km8kIX-8hVs']
+    playlist: ['bIr88lyxXQk', 'Wuir7uvFJ_8', 'nO84UfJUSDc', 'wxgHAZUcsGo']
   },
 ]
--- src/scenes/home/index.js
+++ src/scenes/home/index.js
@@ -1,22 +1,23 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, View, SafeAreaView } from 'react-native';
 import ChannelList from '../../components/organisms/channelList'
 import SpecialsList from '../../components/organisms/specialsList'
 import ScreenDimensionsData from '../../helpers/screenDimensionsData'
 import YouTube from 'react-native-youtube';
+import channels from '../../assets/samples/channelData'
 import { REACT_APP_YOUTUBE_API } from 'react-native-dotenv'
 
-let videoList = ['ErfEnD2WA3A', 'JZnlJ2upJv8', 'Km8kIX-8hVs']
 const Home = () => {
   const isLandscape = ScreenDimensionsData().isLandscape;
-  const [activeChannel, setActiveChannel] = useState(['default', 1])
+  const [activeChannel, setActiveChannel] = useState(['default', channels[0].id])
+  const [videoList, setVideoList] = useState(channels[0].playlist)
 
-  function updateActiveChannel(channel, playlist) { setActiveChannel(channel); videoList = playlist }
+  function updateActiveChannel(channel, playlist) { setActiveChannel(channel); setVideoList(playlist) }
 
   return (
     <SafeAreaView style={[styles.container, isLandscape && styles.containerLandscape]}>
       <View style={[styles.videoWrapper, isLandscape && styles.videoWrappperLandscape]}>
-        <YouTube apiKey={REACT_APP_YOUTUBE_API} videoId={videoList[0]} videoIds={videoList} style={{ alignSelf : 'stretch', flex: 1 }}/>
+        <YouTube loop play apiKey={REACT_APP_YOUTUBE_API} videoId={videoList[0]} videoIds={videoList} style={{ alignSelf : 'stretch', flex: 1 }} />
       </View>
       <View style={[styles.navigationWrapper, isLandscape && styles.navigationWrapperLandscape]}>
         <SpecialsList activeChannel={activeChannel} updateActiveChannel={updateActiveChannel} />
