5bb5af2faf921122de5806422e7768c45d7d7104
Maleur <anna@burti.lv>
2019-12-15 18:54:39 +0200

    Add Async storage

--- src/scenes/home/index.js
+++ src/scenes/home/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { StyleSheet, View, SafeAreaView } from 'react-native';
 import ChannelList from '../../components/organisms/channelList'
 import SpecialsList from '../../components/organisms/specialsList'
@@ -7,42 +7,76 @@ import YouTube from 'react-native-youtube';
 import channels from '../../assets/samples/channelData'
 import theme from '../../assets/styles/theme.style'
 import { REACT_APP_YOUTUBE_API } from 'react-native-dotenv'
+import AsyncStorage from '@react-native-community/async-storage';
 
 const Home = () => {
   const isLandscape = ScreenDimensionsData().isLandscape;
   const [activeChannel, setActiveChannel] = useState(['default', channels[0].id])
-  const [channelPlaylist, setChannelPlaylist] = useState(channels[0].playlist)
   const [playbackList, setPlaybackList] = useState(channels[0].playlist)
+  const [seenData, setSeenData] = useState([])
+  const youtubeRef = useRef(null)
+
+  useEffect(() => {
+    updateSeenData();
+  }, []);
+
+  async function updateSeenData() {
+    const seenVideos = await getVideoCookies()
+    if (seenVideos !== null) { setSeenData(seenVideos.split(',')); }
+  }
 
   function updateActiveChannel(channel, playlist) {
     setActiveChannel(channel);
-    console.log('channel is =>', channel)
-    console.log('acive channel is =>', activeChannel)
-    setChannelPlaylist(playlist);
     updatePlaybackList(playlist);
   }
 
-  function updatePlaybackList(playlist) {
-    const unseenVideos = playlist.filter(video => !watchedData.includes(video))
-    console.log('unseen videos=>', unseenVideos)
-    playbackPlaylist = unseenVideos.concat(playlist)
-    console.log('playbackPlaylist=>', playbackPlaylist)
+  async function updatePlaybackList(playlist) {
+    const seenVideos = await getVideoCookies()
+    const unseenVideos = playlist.filter(video => !seenVideos.includes(video))
+    const playbackPlaylist = unseenVideos.concat(playlist)
     setPlaybackList(playbackPlaylist)
   }
 
-  function updateSeenVideos(e) {
-    console.log('video state changed', e);
+  async function updateSeenVideos(e) {
+    if (e.state === 'playing') {
+      const index = await getVideoIndex()
+      const videoId = playbackList[index]
+      const seenVideos = await getVideoCookies()
+
+      if (seenVideos === null) {
+        newCookie = videoId
+        await updateVideoCookies(newCookie)
+      } else if (!seenVideos.includes(videoId)) {
+        newCookie = seenVideos.concat(',', videoId)
+        await updateVideoCookies(newCookie)
+      }
+
+      await updateSeenData()
+    }
+  }
+
+  async function getVideoIndex() {
+    return youtubeRef.current.getVideosIndex()
+  }
+
+  async function updateVideoCookies(value) {
+    return AsyncStorage.setItem('@seenVideos', value)
+  }
+
+  async function getVideoCookies() {
+    return AsyncStorage.getItem('@seenVideos')
   }
 
   const watchedData = ['B_3pGTXHlTo', 'pbj-gzf9OeA', 'T44ccJQvlx8', '82pVND3efEc', 's-v3dXosB_4', 'P2ppuFwPt6A', 'BfHIw2iFvR4', 'gey-oGUFiJo']
+
   return (
     <SafeAreaView style={[styles.container, isLandscape && styles.containerLandscape]}>
       <View style={[styles.videoWrapper, isLandscape && styles.videoWrappperLandscape]}>
-        <YouTube loop play apiKey={REACT_APP_YOUTUBE_API} onChangeState={(e) => updateSeenVideos(e)} videoIds={playbackList} style={{ alignSelf : 'stretch', flex: 1 }} origin='http://www.youtube.com' />
+        <YouTube loop play apiKey={REACT_APP_YOUTUBE_API} onChangeState={(e) => updateSeenVideos(e)} videoIds={playbackList} style={{ alignSelf : 'stretch', flex: 1 }} origin='http://www.youtube.com' ref={youtubeRef} />
       </View>
       <View style={[styles.navigationWrapper, isLandscape && styles.navigationWrapperLandscape]}>
-        <SpecialsList seenVideos={watchedData} activeChannel={activeChannel} updateActiveChannel={updateActiveChannel} />
-        <ChannelList seenVideos={watchedData} activeChannel={activeChannel} updateActiveChannel={updateActiveChannel} />
+        <SpecialsList seenVideos={seenData} activeChannel={activeChannel} updateActiveChannel={updateActiveChannel} />
+        <ChannelList seenVideos={seenData} activeChannel={activeChannel} updateActiveChannel={updateActiveChannel} />
       </View>
     </SafeAreaView>
   )
